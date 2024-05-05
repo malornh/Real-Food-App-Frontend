d185a450ec2b550029215a4968c009dc97de79f7
malornh <jivko.ivanov14@gmail.com>
2024-05-05 12:05:22 +0300

    Adding raw products to the form

--- src/components/AccountFrom/AccountForm.tsx
+++ src/components/AccountFrom/AccountForm.tsx
@@ -24,7 +24,51 @@ const AccountForm = ({ cards }: Props) => {
   const [selectedCardId, setSelectedCardId] = useState(0);
   const [selectedAccount, setSelectedAccount] = useState(false);
 
-  const foodTypes = ["Meat", "Fruits", "Eggs", "Vegetables", "Honey"];
+  const products= [
+    {
+      Id: 1,
+      Name: "Eggs",
+      Description: "Farm-fresh eggs from free-range chickens",
+      FarmId: 1,
+      UnitOfMeasurement: "Piece",
+      Quantity: 90,
+      PricePerUnit: 0.55,
+      DeliveryRadius: 10.0,
+      MinUnitOrder: 1,
+      DateUpdated: new Date("2024-05-01"),
+      Image: "https://cdn.britannica.com/94/151894-050-F72A5317/Brown-eggs.jpg",
+      Type: "Eggs&Dairy"
+    },
+    {
+      Id: 2,
+      Name: "Strawberries",
+      Description: "Organic ripe strawberries, picked daily",
+      FarmId: 2,
+      UnitOfMeasurement: "Kilogram",
+      Quantity: 30,
+      PricePerUnit: 3.5,
+      DeliveryRadius: 8.0,
+      MinUnitOrder: 1,
+      DateUpdated: new Date("2024-05-02"),
+      Image: "https://images.immediate.co.uk/production/volatile/sites/10/2018/03/2048x1365-Best-strawberries-to-grow-LI1834878-9b05a14.jpg?quality=90&webp=true&crop=9px,11px,2031px,1354px&resize=1200,800",
+      Type: "Fruit"
+    },
+    {
+      Id: 3,
+      Name: "Tomatoes",
+      Description: "Fresh vine-ripened tomatoes",
+      FarmId: 3,
+      UnitOfMeasurement: "Kilogram",
+      Quantity: 40,
+      PricePerUnit: 1.8,
+      DeliveryRadius: 7.0,
+      MinUnitOrder: 1,
+      DateUpdated: new Date("2024-05-03"),
+      Image: "https://www.vincenzosonline.com/userContent/images/Blog/Tomatoes/tomatoes-5.jpg",
+      Type: "Fruit"
+    }
+  ];
+  
 
   const toggleForm = () => {
     setShowForm(prevState => !prevState);
@@ -47,32 +91,30 @@ const AccountForm = ({ cards }: Props) => {
   };
 
   return (
-    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}
-    >
+    <div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
       {selectedAccount ? 'selectedCardBorder' : 'nonSelectedCardBorder'}
-     
-
       <MdAccountCircle  
-      className='buttonStyle' 
-      style={{ right: showForm ? 'calc(40%)' : '20px',  }} 
-      onClick={showForm != true ? toggleForm : ()=> toggleAccount()} 
+        className='buttonStyle' 
+        style={{ right: showForm ? 'calc(40%)' : '20px',  }} 
+        onClick={showForm != true ? toggleForm : ()=> toggleAccount()} 
       />
-      
       {showForm && (
         <div className="formContainerStyle">
           <div className="cardsContainerStyle">
             {cards.map((card) => (
               <div key={card.id} className='cardStyle' onClick={() => handleCardClick(card)}>
-              <img src={card.imgUrl} alt={card.name} style={{ width: '80px', height: '80px', borderRadius: '50px'}} className={card.id === selectedCardId ? 'selectedCardBorder' : 'nonSelectedCardBorder'} />
+                <img src={card.imgUrl} alt={card.name} style={{ width: '80px', height: '80px', borderRadius: '50px'}} className={card.id === selectedCardId ? 'selectedCardBorder' : 'nonSelectedCardBorder'} />
                 <div style={{marginTop: '-10px'}}>{card.name}</div>
               </div>
             ))}
-            <div className='cardStyle' >
+            <div className='cardStyle'>
               <CiCirclePlus className="plusIcon" onClick={handleAddNewCard} />
             </div>
           </div>
           <IoMdCloseCircle className="closeButtonStyle" onClick={toggleForm} />
-          {selectedCard ? <FarmForm types={foodTypes} data={selectedCard} /> : <UserForm/>}
+          <div className="scrollableContent">
+            {selectedCard ? <FarmForm products={products} data={selectedCard} /> : <UserForm/>}
+          </div>
         </div>
       )}
     </div>
--- src/components/FarmForm/FarmForm.tsx
+++ src/components/FarmForm/FarmForm.tsx
@@ -1,56 +1,111 @@
+import { useState } from 'react';
 import { Card } from '../AccountFrom/AccountForm';
 import './FarmForm.css'
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { Text, Tabs, TabList, TabPanels, Tab, TabPanel, NumberInput, NumberInputField, Box } from '@chakra-ui/react'
+import { TbTruckDelivery } from "react-icons/tb";
+
+
+
+interface Product {
+  Id: number;
+  Name: string;
+  Description: string;
+  FarmId: number;
+  UnitOfMeasurement: string;
+  Quantity: number;
+  PricePerUnit: number;
+  DeliveryRadius: number;
+  MinUnitOrder: number;
+  DateUpdated: Date;
+  Image: string;
+  Type: string;
+}
 
 interface Props {
   data: Card,
-  types: string[] 
+  products: Product[]
 }
 
-const FarmForm = ({ data, types }: Props) => {
+const FarmForm = ({ data, products }: Props) => {
+
+  const unique = (arr: any) => [...new Set(arr)];
+
+  const typeList = unique(products.map(p => p.Type));
+
+  const [selectedType, setSelectedType] = useState('');
+  
+  
 
   const handleTabClick={
 
   }
   
   return (
-    <div className="farmFormStyle">
+    <div>
       {data && (
         <div className="farmCardContainer">
           <img src={data.imgUrl} className="farmImage" />
           <div className="farmInfoContainer">
             <h1 className="farmTitle">{data.name}</h1>
-            <p className="farmDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nisi atque velit minus ipsum ratione, enim magnam, sed voluptas similique voluptates laboriosam est cupiditate, commodi accusamus ducimus distinctio dolorum consequuntur!</p>
-            <div className="farmRatingContainer">
-              {data.rating} / 5.0
-            </div>
+            <p className="farmDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+              nisi atque velit minus ipsum ratione, enim magnam, sed voluptas
+              similique voluptates laboriosam est cupiditate, commodi accusamus
+              ducimus distinctio dolorum consequuntur!
+            </p>
+            <div className="farmRatingContainer">{data.rating} / 5.0</div>
           </div>
         </div>
       )}
-       <Tabs>
-      <TabList className='foodTabMenu'>
-        {types.map((type, index) => (
-          <Tab className='tab' key={index}>{type}</Tab>
-        ))}
-      </TabList>
+      <Tabs>
+        <TabList className="foodTabMenu">
+          {typeList.map((type) => (
+            <Tab
+              className="tab"
+              key={type as string}
+              onClick={() => setSelectedType(type as string)}>
+              {type as string}
+            </Tab>
+          ))}
+        </TabList>
 
-      <TabPanels>
-        {types.map((type, index) => (
-          <TabPanel className='tabPanel' key={index}>
-            <div style={{display: 'flex'}}>
-              <div>
-                <img style={{width: '80px', height: '80px', borderRadius: '15px', padding: '10px'}} src={"https://www.bta.bg/upload/2870506/Syngenta_Barosor_1.jpg?l=1000&original=013bbbec5fca1125637321752b8cfa6a81e98933"} />
-              </div>
-              <div style={{color: 'black'}}>
-                Tomatoes
-              </div>
-            </div>
-          </TabPanel>
-        ))}
-      </TabPanels>
-    </Tabs>
-
-      </div>
+        <TabPanels>
+          {typeList.map((type) => (
+            <TabPanel className="tabPanel" key={type as string}>
+              {products
+                .filter((p) => p.Type === type)
+                .map((p) => (
+                  <div
+                    key={p.Id}
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      marginBottom: "10px",
+                      background: "rgba(254, 216, 65, 0.8)",
+                      borderRadius: "5px",
+                    }}>
+                    <div
+                      style={{ display: "flex", justifyContent: "flex-start" }}>
+                      <img
+                        style={{
+                          width: "130px",
+                          height: "130px",
+                          borderRadius: "10px",
+                          marginTop: "5px",
+                          marginLeft: "5px",
+                          marginBottom: "5px",
+                          padding: "5px",
+                        }}
+                        src={p.Image}
+                      />
+                    </div>
+                  </div>
+                ))}
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
+    </div>
   );
 }
 
